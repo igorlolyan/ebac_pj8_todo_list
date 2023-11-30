76b7aede121e67d56944f2734fe422704d6706a4
igorlolyan <igor.lol.yan@gmail.com>
2023-11-30 16:56:50 -0300

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,55 @@
+const nomeDaTarefa = $('#inputNomeDaTarefa');
+const formulário = $('#formNomeDaTarefa');
+
+const minhaLista = $('ul');
+
+var arrayTarefas = [];
+var linhas = '';
+
+formulário.on('submit', function(e){
+    e.preventDefault();
+
+    addLinhas();
+    atualizaLista();
+})
+
+
+function addLinhas() {
+    if (arrayTarefas.includes(nomeDaTarefa.val())) {
+        alert(`Essa tarefa já foi inserida meu querido.\nDigite outra tarefa!`);
+    } else {
+        arrayTarefas.push(nomeDaTarefa.val());
+
+        var linha = 
+        `
+            <label>
+                <li>
+                    <input type='checkbox'>
+                    ${nomeDaTarefa.val()}
+                </li>
+            </label>
+        `;
+
+        linhas += linha;
+    }
+}
+
+function atualizaLista() {
+
+    minhaLista.html(linhas);
+
+}
+
+minhaLista.on('click', 'li', function (e) {
+    const target = $(e.target);
+
+    if (target.is('input[type="checkbox"]')) {
+        // Se o clique foi no checkbox
+        target.closest('li').toggleClass('checked', target.prop('checked'));
+    } else if (target.is('label')) {
+        // Se o clique foi no texto da tarefa (dentro do label)
+        const checkbox = target.find('input[type="checkbox"]');
+        checkbox.prop('checked', !checkbox.prop('checked'));
+        target.closest('li').toggleClass('checked', checkbox.prop('checked'));
+    }
+});
